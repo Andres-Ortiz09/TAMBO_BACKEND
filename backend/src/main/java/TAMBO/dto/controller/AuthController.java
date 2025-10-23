5f636f05c5ccf5dc4f06c22c7d150d4256a3d3df
Andres-Ortiz09 <ortizloyolaandres@gmail.com>
2025-10-23 03:30:24 -0500

    crear controlador de autenticación con endpoints para login y registro

---
+++ backend/src/main/java/TAMBO/dto/controller/AuthController.java
@@ -0,0 +1,28 @@
+package TAMBO.dto.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthServices authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/registro")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.ok(authService.registro(request));
+    }
+}
