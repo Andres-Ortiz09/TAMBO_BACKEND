b96a26090d180f4082a5df2d36a51065496e100c
Andres-Ortiz09 <ortizloyolaandres@gmail.com>
2025-10-23 03:17:58 -0500

    aqui mapee la tabla idrol

--- backend/src/main/java/TAMBO/entity/Rol.java
+++ backend/src/main/java/TAMBO/entity/Rol.java
@@ -1,10 +1,15 @@
 package TAMBO.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "rol")
@@ -16,6 +21,7 @@ public class Rol {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol") // ✅ mapeo exacto
     private Integer idRol;
 
     @Column(nullable = false, unique = true)
